40e674f823443621c0b5fa59ad1dac4d01c59aae
Jim124 <fengpingan065@gmail.com>
2024-06-27 11:53:51 +0800

    custom fonts

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,12 @@ import Navagation from '@/app/_components/Navagation';
 import Logo from '@/app/_components/Logo';
 
 import '@/app/_styles/globals.css';
+import { Josefin_Sans } from 'next/font/google';
+
+const josefinsans = Josefin_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export const metadata = {
   title: {
@@ -15,7 +21,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className='bg-primary-950 text-primary-100 min-h-screen'>
+      <body
+        className={`${josefinsans.className} bg-primary-950 text-primary-100 min-h-screen`}
+      >
         <header>
           <Logo />
           <Navagation />
