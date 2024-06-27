3d8938bd094c43a1517239f820a1aff19a90c165
Jim124 <fengpingan065@gmail.com>
2024-06-27 14:12:02 +0800

    add reservation and profile page file

--- app/account/page.js
+++ app/account/page.js
@@ -3,7 +3,11 @@ export const metadata = {
 };
 
 function Page() {
-  return <div>this is a account page.</div>;
+  return (
+    <h2 className='font-semibold text-2xl text-accent-400 mb-7'>
+      Welcome back
+    </h2>
+  );
 }
 
 export default Page;
