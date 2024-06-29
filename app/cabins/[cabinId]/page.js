298d49bec77afe3a81c5728023b74720c619419d
Jim124 <fengpingan065@gmail.com>
2024-06-29 15:00:44 +0800

    add generateStaticParams function

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 
-import { getCabin } from '@/app/_lib/data-service';
+import { getCabin, getCabins } from '@/app/_lib/data-service';
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from '@heroicons/react/24/solid';
 
 export async function generateMetadata({ params }) {
@@ -8,6 +8,11 @@ export async function generateMetadata({ params }) {
   return { title: `Cabin ${name}` };
 }
 
+export async function generateStaticParams() {
+  const cabins = await getCabins();
+  const ids = cabins.map((cabin) => ({ cabinId: String(cabin.id) }));
+  return ids;
+}
 export default async function Page({ params }) {
   const cabin = await getCabin(params.cabinId);
   const { id, name, maxCapacity, regularPrice, discount, image, description } =
