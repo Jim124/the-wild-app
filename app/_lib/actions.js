6654fbc020b030ea7eab39223877217f5f264f4c
Jim124 <fengpingan065@gmail.com>
2024-07-02 12:14:26 +0800

    edit reservation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,7 +3,7 @@
 import { supabase } from './supabase';
 import { auth, signIn, signOut } from './auth';
 import { revalidatePath } from 'next/cache';
-import { deleteBooking, getBookings } from './data-service';
+import { deleteBooking, getBookings, updateBooking } from './data-service';
 
 export async function signInAction() {
   await signIn('google', { redirectTo: '/account' });
@@ -46,5 +46,28 @@ export async function deleteReservationByBookingId(bookingId) {
     throw new Error(error.message);
   }
 
+  revalidatePath('/account/reservations');
+}
+
+export async function updateReservation(formData) {
+  const bookingId = formData.get('bookingId');
+  console.log(typeof bookingId);
+
+  const observations = formData.get('observations');
+  const numGuests = formData.get('numGuests');
+  const session = await auth();
+  if (!session) throw new Error('You must be logged in');
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookIds = guestBookings.map((booking) => booking.id);
+  if (!guestBookIds.includes(Number(bookingId)))
+    throw new Error('You are not allowded to delete this booking');
+  try {
+    const { data, error } = await updateBooking(bookingId, {
+      observations,
+      numGuests,
+    });
+  } catch (error) {
+    throw new Error(error.message);
+  }
   revalidatePath('/account/reservations');
 }
